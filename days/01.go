4429038b1f6834e3f73565d4a5c6bbb33ba1b92f
tkwu <tkwu@ncsu.edu>
2023-02-09 23:13:23 -0500

    fixed re-declare func error

--- days/01.go
+++ days/01.go
@@ -4,40 +4,37 @@ import (
 	"strconv"
 )
 
-func partOne(input string) string {
-	var floor int = 0
-	for _, r := range input {
-		if r == '(' {
-			floor++
-		} else {
-			floor--
-		}
-	}
-	return strconv.Itoa(floor)
-}
-
-func partTwo(input string) string {
-	var floor int = 0
-	for i, r := range input {
-		if r == '(' {
-			floor++
-		} else {
-			floor--
-		}
-
-		if floor == -1 {
-			return strconv.Itoa(i + 1)
-		}
-	}
-	return ""
-}
-
-var day01 Day = Day{
-	DayNum:  1,
-	PartOne: partOne,
-	PartTwo: partTwo,
-}
-
 func init() {
-	RegisterDay("Day 01", day01)
+	var day Day = Day{
+		DayNum: 1,
+
+		PartOne: func(input string) string {
+			var floor int = 0
+			for _, r := range input {
+				if r == '(' {
+					floor++
+				} else {
+					floor--
+				}
+			}
+			return strconv.Itoa(floor)
+		},
+
+		PartTwo: func(input string) string {
+			var floor int = 0
+			for i, r := range input {
+				if r == '(' {
+					floor++
+				} else {
+					floor--
+				}
+
+				if floor == -1 {
+					return strconv.Itoa(i + 1)
+				}
+			}
+			return ""
+		},
+	}
+	RegisterDay("Day 01", &day)
 }
