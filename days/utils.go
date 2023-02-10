02a7f9800c945a62569efe80167bb8c6083442ae
tkwu <tkwu@ncsu.edu>
2023-02-09 22:13:49 -0500

    format output of executing day and part

--- days/utils.go
+++ days/utils.go
@@ -1,15 +1,17 @@
 package days
 
-type task func(string) (string, error)
+type Task func(string) string
 
 type Day struct {
 	DayNum  int
-	PartOne task
-	PartTwo task
+	PartOne Task
+	PartTwo Task
 }
 
+// Holds the functions for each aoc day
 var AOCDays map[string]Day = map[string]Day{}
 
+// Adds the day to the map
 func RegisterDay(dayFmt string, day Day) {
 	AOCDays[dayFmt] = day
 }
--- main.go
+++ main.go
@@ -5,40 +5,64 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"time"
 )
 
+// Format inspired by https://github.com/fspoettel/advent-of-code-rust
 func main() {
-    var args []string = os.Args
-    if len(args) != 4 {
-        invalidArgs()
-    }
-    fmt.Println(args)
+	// Args error checking
+	var args []string = os.Args
+	if len(args) != 4 {
+		invalidArgs()
+	}
 
-    var dayNum int
-    var err error
-    dayNum, err = strconv.Atoi(os.Args[1])
-    if err != nil {
-        invalidArgs()
-    }
+	var dayNum int
+	var err error
+	dayNum, err = strconv.Atoi(os.Args[1])
+	if err != nil {
+		invalidArgs()
+	}
 
-    var part string = os.Args[2]
-    if part != "p1" && part != "p2" && part != "both" {
-        invalidArgs()
-    }
+	var part string = os.Args[2]
+	if part != "p1" && part != "p2" && part != "both" {
+		invalidArgs()
+	}
 
-    var task string = os.Args[3]
-    if task != "solve" && task != "test" {
-        invalidArgs()
-    }
+	var fileContents []byte
+	fileContents, err = os.ReadFile(os.Args[3])
+	if err != nil {
+		fmt.Printf("Error reading file: %s\n", err)
+		os.Exit(1)
+	}
 
-    var dayFmt string = fmt.Sprintf("day%02d", dayNum)
-    fmt.Println(dayFmt)
-    var day days.Day = days.AOCDays[dayFmt]
+	var dayFmt string = fmt.Sprintf("Day %02d", dayNum)
+	var day days.Day = days.AOCDays[dayFmt]
 
-    fmt.Println(day.PartOne("asdfasdf"))
+	fmt.Printf("----------\n"+
+		"| %s |\n"+
+		"----------\n", dayFmt)
+
+	var input string = string(fileContents)
+	if part == "p1" || part == "both" {
+		fmt.Println("\n🎄 Part 1 🎄")
+		benchmarkTask(day.PartOne, input)
+	}
+	if part == "p2" || part == "both" {
+		fmt.Println("\n🎄 Part 2 🎄")
+		benchmarkTask(day.PartTwo, input)
+	}
+}
+
+// Benchmark function
+func benchmarkTask(task days.Task, input string) {
+	var start time.Time = time.Now()
+	var output string = task(string(input))
+	var duration time.Duration = time.Since(start)
+
+	fmt.Printf("%s (elapsed: %s)\n", output, duration)
 }
 
 func invalidArgs() {
-    fmt.Println("Usage: go run main.go <day-number> <p1|p2|both> <solve|test>")
-    os.Exit(1)
+	fmt.Println("Usage: go run main.go <day-number> <p1|p2|both> <file-path-input>")
+	os.Exit(1)
 }
